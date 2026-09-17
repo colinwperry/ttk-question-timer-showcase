459e92d77c75840bb928ee2ba723db94d2837f3b
Colin Perry <colinperry922@gmail.com>
2026-09-17 19:48:14 -0400

    implemented timer and window logic - implemented proper logic and formatting for the timer - properly displayed and continually updated timer in Tk window

--- app.py
+++ app.py
@@ -1,5 +1,36 @@
 from tkinter import *
 from tkinter import ttk
-import datetime
+import time 
 
-print("hello world")
+root = Tk()
+
+frame = ttk.Frame(root, padding=10)
+frame.grid()
+
+timer_length = 120 # in seconds
+
+def timerStart():
+    global start_time; start_time = time.time()
+    global timer_length; timer_length = 120
+    timer_button.configure(text="Stop Timer", command=timerStop)
+    root.after(0, incrementTimer)
+
+def timerStop():
+    global timer_length; timer_length = 0
+    timer_label.configure(text="0:00")
+    timer_button.configure(text="Start Timer", command=timerStart)
+
+def incrementTimer():
+    if timer_length > 0:
+        root.after(1000, incrementTimer)
+        proper_time = int(start_time - time.time() + timer_length)
+        minute, second = proper_time // 60, proper_time % 60
+        timer_label.configure(text=f"{minute}:{second:02}") 
+
+timer_label = ttk.Label(frame, text="0:00")
+timer_label.grid(column=0, row=0)
+
+timer_button = ttk.Button(frame, text="Start Timer", command=timerStart)
+timer_button.grid(column=0, row=1)
+
+root.mainloop()
