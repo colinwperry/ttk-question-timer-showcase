6ecca98056206c8504b294c50c497a13e98f145f
Colin Perry <colinperry922@gmail.com>
2026-09-18 13:01:08 -0400

    --h

--- app.py
+++ app.py
@@ -7,17 +7,31 @@ root = Tk()
 frame = ttk.Frame(root, padding=10)
 frame.grid()
 
-timer_length = 120 # in seconds
+timer_label = ttk.Label(frame, text="0:00")
+timer_label.grid(column=0, row=0)
+
+question_container = {
+        1: ["What is a variable?", "A way to save and use information."],
+        2: ["print(\"Hello World!\")", "prints Hello World!"]
+        }
+
+logic_question_button = ttk.Button(frame, text="Logic Question", command=lambda: questionStart(1))
+logic_question_button.grid(column=0, row=1)
+
+code_question_button = ttk.Button(frame, text="Code Question", command=lambda: questionStart(2))
+code_question_button.grid(column=1,row=1)
+
+def questionStart(question_id):
+    print(question_container[question_id][0])
 
 def timerStart():
     global start_time; start_time = time.time()
     global timer_length; timer_length = 120
     timer_button.configure(text="Stop Timer", command=timerStop)
-    root.after(0, incrementTimer)
+    incrementTimer()
 
 def timerStop():
     global timer_length; timer_length = 0
-    timer_label.configure(text="0:00")
     timer_button.configure(text="Start Timer", command=timerStart)
 
 def incrementTimer():
@@ -26,11 +40,5 @@ def incrementTimer():
         proper_time = int(start_time - time.time() + timer_length)
         minute, second = proper_time // 60, proper_time % 60
         timer_label.configure(text=f"{minute}:{second:02}") 
-
-timer_label = ttk.Label(frame, text="0:00")
-timer_label.grid(column=0, row=0)
-
-timer_button = ttk.Button(frame, text="Start Timer", command=timerStart)
-timer_button.grid(column=0, row=1)
 
 root.mainloop()
